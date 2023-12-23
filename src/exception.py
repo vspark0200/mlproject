22eb9f20758a450b817bb8cbafea11cb1e9decd1
haresh <hareshshivakumar@gmail.com>
2023-12-23 00:46:12 +0000

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def error_message_detail(error, error_detail: sys):
+    _, _, error_detail.exc.info()
+    file_name = exc.tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{0}] line numeber [{1}] error message[{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+
+        return error_message
+    )
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super.__init__(error_message)
+        self.error_message = error_message_detail(
+            error, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
